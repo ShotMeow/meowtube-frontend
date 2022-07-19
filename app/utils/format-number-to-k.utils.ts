22cb0035a1a56a5eee67b6468135977a5fec112d
ShotMeow <shotmeow@gmail.com>
2022-07-19 18:45:02 +0300

    Fix util name

--- app/utils/format-number-to-k.utils.ts
+++ app/utils/format-number-to-k.utils.ts
@@ -1,4 +1,4 @@
-export function formatNumberToK(num: number) {
+export function formatNumberToKUtils(num: number) {
 	if (num >= 1000000000)
 		return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G'
 	if (num >= 1000000)
