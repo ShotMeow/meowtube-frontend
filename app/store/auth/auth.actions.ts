4c63236bbb4b26b41555d1543a5e44e2fb0b0a43
ShotMeow <shotmeow@gmail.com>
2022-07-19 19:35:13 +0300

    Configure Redux Store

---
+++ app/store/auth/auth.actions.ts
@@ -0,0 +1,41 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { toastr } from 'react-redux-toastr'
+
+import { IAuthFields } from '@/components/Layout/Header/AuthForm/AuthForm.interface'
+
+import { IAuthData } from '@/services/auth/auth.helper'
+import { AuthService } from '@/services/auth/auth.service'
+
+import { toastError } from '@/utils/api.utils'
+
+export const register = createAsyncThunk<IAuthData, IAuthFields>(
+	'auth/register',
+	async ({ email, password }, thunkAPI) => {
+		try {
+			const response = await AuthService.register(email, password)
+			toastr.success('Регистрация', 'Успешно выполнена')
+			return response
+		} catch (error) {
+			toastError(error)
+			return thunkAPI.rejectWithValue(error)
+		}
+	}
+)
+
+export const login = createAsyncThunk<IAuthData, IAuthFields>(
+	'auth/login',
+	async ({ email, password }, thunkAPI) => {
+		try {
+			const response = await AuthService.login(email, password)
+			toastr.success('Вход в систему', 'Успешно выполнена')
+			return response
+		} catch (error) {
+			toastError(error)
+			return thunkAPI.rejectWithValue(error)
+		}
+	}
+)
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+	return {}
+})
