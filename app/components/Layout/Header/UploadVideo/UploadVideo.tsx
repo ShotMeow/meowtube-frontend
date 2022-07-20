293cc3622ab071aeb681ab3e3a9c0c8ae5276877
ShotMeow <shotmeow@gmail.com>
2022-07-20 19:09:05 +0300

    Work in Progress

--- app/components/Layout/Header/UploadVideo/UploadVideo.tsx
+++ app/components/Layout/Header/UploadVideo/UploadVideo.tsx
@@ -1,7 +1,30 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+
+import { videoApi } from '@/store/api/video.api'
+
+import stylesIcon from '../IconsRight/IconsRight.module.scss'
 
 const UploadVideo: FC = () => {
-	return <div></div>
+	const [isOpen, setIsOpen] = useState(false)
+	const [videoId, setVideoId] = useState<number>(0)
+
+	const [createVideo, { isLoading }] = videoApi.useCreateVideoMutation()
+	return (
+		<>
+			<button
+				className={stylesIcon.button}
+				disabled={isLoading}
+				onClick={() => {
+					createVideo()
+						.unwrap()
+						.then(id => {
+							setVideoId(+id)
+							setIsOpen(true)
+						})
+				}}
+			></button>
+		</>
+	)
 }
 
 export default UploadVideo
